0d1f3834bf55b4ffd93b570a342662a3d4782448
Bosun Olanrewaju <bosunolanrewaju@gmail.com>
2014-08-13 12:11:17 +0100

    User registration with password field added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+	validates :username,
+				presence: true,
+				uniqueness: true,
+				length: {minimum: 4, maximum: 20}
+
+			has_secure_password
+
+	validates :password_confirmation,
+				presence: true
 end
